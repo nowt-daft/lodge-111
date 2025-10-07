f5531b45d7ccb4cae1bd93ef9b53b7287a61bb97
nowt-daft <dumpstergoon@protonmail.com>
2025-10-07 23:00:14 +0100

    howdy

--- wp-content/themes/testtheme/patterns/meetings.php
+++ wp-content/themes/testtheme/patterns/meetings.php
@@ -6,20 +6,6 @@
  * Description: Render Meetings
 */
 
-$latest_post = get_posts([
-	"post_type" => "events",
-	"posts_per_page" => 1
-]);
-
-if (empty($latest_post))
-	return;
-
-$latest_post = $latest_post[0];
-$date = get_post_meta($latest_post->ID, 'event_date', true);
-$time = get_post_meta($latest_post->ID, 'event_time', true);
-
-$date_parts = explode('-', $date);
-
 $months = [
 	"___",
 	"JAN",
@@ -36,25 +22,52 @@ $months = [
 	"DEC"
 ];
 
+$posts = get_posts([
+	"post_type" => "events",
+	"posts_per_page" => 3
+]);
+
+if (empty($posts))
+	return;
+
 ?>
 <section class="meetings">
-	<header>
-		<h2><?php echo $latest_post->post_title ?></h2>
-	</header>
-	<div class="content">	
-		<time
-			datetime="<?php echo $date ?>T<?php $time ?>"
-		>
-			<span class="month">
-				<?php echo $months[(int)$date_parts[1]] ?>
-			</span>
-			<span class="day">
-				<?php echo $date_parts[2] ?>
-			</span>
-		</time>
-		<p>
-			<?php echo $latest_post->post_content ?>
-		</p>
-	</div>
+	<ul>
+		<?php foreach ($posts as $post) : ?>
+		<li>
+			<article>
+
+				<?php
+					$ID = $post->ID;
+
+					$date = get_post_meta($ID, 'event_date', true);
+					$time = get_post_meta($ID, 'event_time', true);
+
+					$date_parts = explode('-', $date);
+				?>
+
+				<header>
+					<h2><?php echo $post->post_title ?></h2>
+				</header>
+				<div class="content">	
+					<time
+						datetime="<?php echo $date ?>T<?php $time ?>"
+					>
+						<span class="month">
+							<?php echo $months[(int)$date_parts[1]] ?>
+						</span>
+						<span class="day">
+							<?php echo $date_parts[2] ?>
+						</span>
+					</time>
+					<p>
+						<?php echo $post->post_content ?>
+					</p>
+				</div>
+
+			</article>
+		</li>
+		<?php endforeach ?>
+	</ul>
 </section>
 <hr />
